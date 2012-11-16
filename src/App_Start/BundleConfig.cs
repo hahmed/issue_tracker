b954c9ab7e79d6f112dc916c2b773e504918393d
haroon <haroon.ahmed25@gmail.com>
2012-11-16 15:36:52 +0000

    we got issues png added

--- src/App_Start/BundleConfig.cs
+++ src/App_Start/BundleConfig.cs
@@ -10,9 +10,9 @@ namespace IssueTracker
         {
 
             bundles.Add(new StyleBundle("~/assets/app_css").Include(
-                        "~/assets/css/bootstrap.css",
-                        "~/assets/css/app.css"
-                        ));
+                "~/assets/css/bootstrap.css",
+                "~/assets/css/app.css"
+               ));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -20,16 +20,16 @@ namespace IssueTracker
             bundles.Add(new ScriptBundle("~/assets/app_js").Include(
                         "~/assets/js/modernizr-*",
                         "~/assets/js/jquery-{version}.js",
-                        "~/assets/js/jquery.unobtrusive*",
-                        "~/assets/js/jquery.validate*",
-                        "~/assets/js/jquery.validate*",
-                        "~/assets/js/underscore.js",
-                        "~/assets/js/backbone.js",
-                        "~/assets/js/bootstrap.js",
-                        "~/assets/js/backbone-articulation.js"
+                "~/assets/js/jquery.unobtrusive*",
+                "~/assets/js/jquery.validate*",
+                "~/assets/js/jquery.validate*",
+                "~/assets/js/underscore.js",
+                "~/assets/js/backbone.js",
+                "~/assets/js/bootstrap.js",
+                "~/assets/js/backbone-articulation.js"
                         ));
 
-            BundleTable.EnableOptimizations = false;           
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
