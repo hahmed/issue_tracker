22cd932ad13be14021984c248b17750859c8865a
haroon <haroon.ahmed25@gmail.com>
2012-11-16 15:46:59 +0000

    ui prepped

--- src/Controllers/IssuesController.cs
+++ src/Controllers/IssuesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IssueTracker.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,9 +14,34 @@ namespace IssueTracker.Controllers
         [HttpGet]
         public ActionResult List()
         {
-
-
-            return View();
+            var list = new List<Issues>();
+            list.Add(new Issues
+            {
+                Description = "",
+                When = DateTime.Now,
+                Title = "Something Happened",
+                TypeOfIssue = IssueType.Bug
+            });
+            list.Add(new Issues
+            {
+                Description = "",
+                When = DateTime.Now,
+                Title = "Logo get",
+                TypeOfIssue = IssueType.Comment
+            }); list.Add(new Issues
+            {
+                Description = "",
+                When = DateTime.Now,
+                Title = "show some list",
+                TypeOfIssue = IssueType.TODO
+            }); list.Add(new Issues
+            {
+                Description = "",
+                When = DateTime.Now,
+                Title = "take a break, have a kit kat",
+                TypeOfIssue = IssueType.Archive
+            });
+            return View(list);
         }
 
     }
---
+++ src/Models/Issues.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IssueTracker.Models
+{
+    public class Issues
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime When { get; set; }
+
+        public IssueType TypeOfIssue { get; set; }
+    }
+
+    public enum IssueType
+    {
+        Bug,
+        Comment,
+        Important,
+        TODO,
+        Archive
+    }
+
+
+}
